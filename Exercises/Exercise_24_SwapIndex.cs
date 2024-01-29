46e560e6f9beaf5c21434c4707ab225be6665eaf
Marcellus Farias <marcelluscfarias@gmail.com>
2024-01-29 12:53:37 -0300

    Exercise 24 done

---
+++ Exercises/Exercise_24_SwapIndex.cs
@@ -0,0 +1,30 @@
+﻿using StructuresLibrary;
+
+namespace Exercises;
+
+public partial class Exercise
+{
+    // Given a linked list, swap every two adjacent nodes and return its head.
+    // You must solve the problem without modifying the values in the list's
+    // nodes (i.e., only nodes themselves may be changed.)
+    public static ListNode? SweepPairs(ListNode head)
+    {
+        if (head is null)
+        {
+            return null; 
+        }
+
+        ListNode? currentNode = head;
+
+        while(currentNode is not null && currentNode.next is not null)
+        {
+            var temp = currentNode.val;
+            currentNode.val = currentNode.next.val;
+            currentNode.next.val = temp;
+
+            currentNode = currentNode.next.next;
+        }
+        
+        return currentNode;
+    }
+}
