8867afe3a030dcab6b9c6aa299e51c829c13cde6
Marcellus Farias <marcelluscfarias@gmail.com>
2024-01-30 11:16:46 -0300

    Exercise 25

---
+++ Exercises.Tests/Test_25_ReverseNodesInKGroup.cs
@@ -0,0 +1,63 @@
+﻿using Exercises;
+using StructuresLibrary;
+
+namespace UnitTests;
+
+public class ExerciseTests_25_ReverseNodesInKGroup
+{
+    public static IEnumerable<object?[]> Inputs()
+    {
+        // Case 1
+        yield return new object?[]
+        {
+            ListNodeHelper.CreateFromArray(new int[] { 1,2,3,4,5 }),
+            2,
+            ListNodeHelper.CreateFromArray(new int[] { 2,1,4,3,5 })
+        };
+
+        // Case 2
+        yield return new object?[]
+        {
+            ListNodeHelper.CreateFromArray(new int[] { 1,2,3,4,5 }),
+            3,
+            ListNodeHelper.CreateFromArray(new int[] { 3,2,1,4,5 })
+        };
+
+        // Case 3
+        yield return new object?[]
+        {
+            ListNodeHelper.CreateFromArray(new int[] { 1,2,3,4,5 }),
+            1,
+            ListNodeHelper.CreateFromArray(new int[] { 1,2,3,4,5 })
+        };
+
+        // Case 4
+        yield return new object?[]
+        {
+            ListNodeHelper.CreateFromArray(new int[] { 1,2,3,4,5 }),
+            5,
+            ListNodeHelper.CreateFromArray(new int[] { 5,4,3,2,1 })
+        };
+
+        // Case 4
+        yield return new object?[]
+        {
+            ListNodeHelper.CreateFromArray(new int[] { 1,2,3,4,5 }),
+            4,
+            ListNodeHelper.CreateFromArray(new int[] { 4,3,2,1,5 })
+        };
+    }
+
+
+
+    [Theory]
+    [MemberData(nameof(Inputs))]
+    public void ReverseKLists_ShouldReturnExpectedOutput_WhenValidInputIsProvided(ListNode input, int k, ListNode expectedOutput)
+    {
+        // Act
+        var output = Exercise.ReverseKGroup(input, k);
+
+        // Assert
+        Assert.Equal(expectedOutput, output);
+    }
+}
---
+++ Exercises/Exercise_25_ReverseNodesInKGroup.cs
@@ -0,0 +1,61 @@
+﻿using StructuresLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises;
+
+public partial class Exercise
+{
+
+    public static ListNode? ReverseKGroup(ListNode head, int k)
+    {
+        if (head is null)
+        {
+            return null;
+        }
+
+        var node = head;
+
+        while (node is not null)
+        {
+            var stack = GetNextKValues(node, k);
+
+            if (stack is null)
+            {
+                return head;
+            }
+
+            while (stack.TryPop(out int value))
+            {
+                node!.val = value;
+                node = node.next;
+            }
+        }
+
+        return head;
+    }
+
+    private static Stack<int>? GetNextKValues(ListNode head, int k)
+    {
+        Stack<int> values = new Stack<int>();
+        var node = head;
+        for (int i = 0; i < k; i++)
+        {
+            if (node is null)
+            {
+                return null;
+            }
+            else
+            {
+                values.Push(node.val);
+                node = node.next;
+            }
+        }
+
+        return values;
+    }
+
+}
